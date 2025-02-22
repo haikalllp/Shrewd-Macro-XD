fa00ad8098f5e6f80c9ec4d591f16777a19281bd
Haikal PC / Shrewdy <haikalfakhri24@gmail.com>
2025-02-22 20:35:41 +0800

    cleaned

--- obj/Debug/net6.0-windows/MouseMacro.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/MouseMacro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Task Management and Note Taking Application")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bcffc30bfca5998ae4f313948e550927ad5e69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b90dccff3a26268cde806f3c00b1d865cca245")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
